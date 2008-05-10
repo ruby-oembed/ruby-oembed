1b5ab0c40fae424b48c4474caec15a0a346f46ed
Magnus Holm <judofyr@gmail.com>
2008-05-10 22:49:40 +0200

    Added some providers

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -11,9 +11,29 @@ module OEmbed
         end
       end
       
+      def register_all
+        register(Flickr, Viddler, Qik, Pownce, Revision3)
+      end
+      
       def find(url)
         @@urls[@@urls.keys.detect { |u| u =~ url }] || false
       end
     end
+    
+    # Custom providers:
+    Flickr = OEmbed::Provider.new("http://www.flickr.com/services/oembed/")
+    Flickr << "http://*.flickr.com/*"     
+    
+    Viddler = OEmbed::Provider.new("http://lab.viddler.com/services/oembed/")
+    Viddler << "http://*.viddler.com/*"
+    
+    Qik = OEmbed::Provider.new("http://qik.com/api/oembed.{format}")
+    Qik << "http://qik.com/*"
+    
+    Pownce = OEmbed::Provider.new("http://api.pownce.com/2.1/oembed.{format}")
+    Pownce << "http://*.pownce.com/*"
+    
+    Revision3 = OEmbed::Provider.new("http://revision3.com/api/oembed/")
+    Revision3 << "http://*.revision3.com/*"
   end
 end
