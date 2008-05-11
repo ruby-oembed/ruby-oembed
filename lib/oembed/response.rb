9620f797598cc7ee72470eab095cf806ff1a4383
Magnus Holm <judofyr@gmail.com>
2008-05-11 22:39:16 +0200

    Getting rid of #method_missing

--- lib/oembed/response.rb
+++ lib/oembed/response.rb
@@ -1,18 +1,29 @@
 module OEmbed
   class Response
+    METHODS = [:initialize, :provider, :field, :fields, :__send__, :__id__]
     attr_reader :fields, :provider
     
     def initialize(json, provider)
       @fields = JSON.load(json)
       @provider = provider
+      define_methods!
     end
     
     def field(m)
       @fields[m.to_s]
     end
     
-    def method_missing(meth, *args, &blk)
-      field(meth) || super
+    private
+    
+    def define_methods!
+      @fields.keys.each do |key|
+        next if METHODS.include?(key.to_sym)
+        class << self
+          self
+        end.send(:define_method, key) do
+          @fields[key]
+        end
+      end
     end
   end
 end
