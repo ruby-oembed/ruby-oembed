61d5c9f1e8eccff8ea6d145ab60df6581e9eaf7a
Kyle Slattery <kyle@kyleslattery.com>
2008-07-09 00:53:31 -0400

    removing test.rb

--- test.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'lib/oembed'
-
-OEmbed::Providers.register_all
-puts OEmbed::Providers.get('http://www.viddler.com/explore/kyleslat/videos/127/', :maxwidth => 153).html_code
