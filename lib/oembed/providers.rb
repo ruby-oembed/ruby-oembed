74582b3f1cd2e02ce69d0ca376e7960d2cfce1f0
Colin Shea <colin@evaryont.me>
2010-02-05 22:25:49 -0500

    Add a bunch of OEmbed providers, and re organize the list
    Besides the new additions, the list now matches the order on http://oembed.com
    
    I also added the URL targets to OohEmbed. Fallback should still work, however.

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -75,6 +75,9 @@ module OEmbed
     end
 
     # Custom providers:
+    Youtube = OEmbed::Provider.new("http://www.youtube.com/oembed/")
+    Youtube << "http://*.youtube.com/*"
+
     Flickr = OEmbed::Provider.new("http://www.flickr.com/services/oembed/")
     Flickr << "http://*.flickr.com/*"
 
@@ -83,9 +86,7 @@ module OEmbed
 
     Qik = OEmbed::Provider.new("http://qik.com/api/oembed.{format}")
     Qik << "http://qik.com/*"
-
-    Pownce = OEmbed::Provider.new("http://api.pownce.com/2.1/oembed.{format}")
-    Pownce << "http://*.pownce.com/*"
+    Qik << "http://qik.com/video/*"
 
     Revision3 = OEmbed::Provider.new("http://revision3.com/api/oembed/")
     Revision3 << "http://*.revision3.com/*"
@@ -93,13 +94,53 @@ module OEmbed
     Hulu = OEmbed::Provider.new("http://www.hulu.com/api/oembed.{format}")
     Hulu << "http://www.hulu.com/watch/*"
 
-    Youtube = OEmbed::Provider.new("http://www.youtube.com/oembed/")
-    Youtube << "http://*.youtube.com/*"
-
     Vimeo = OEmbed::Provider.new("http://www.vimeo.com/api/oembed.{format}")
     Vimeo << "http://*.vimeo.com/*"
     Vimeo << "http://*.vimeo.com/groups/*/videos/*"
 
+    Pownce = OEmbed::Provider.new("http://api.pownce.com/2.1/oembed.{format}")
+    Pownce << "http://*.pownce.com/*"
+
+    # A general end point, which then calls other APIs and returns OEmbed info
     OohEmbed = OEmbed::Provider.new("http://oohembed.com/oohembed/")
+    OohEmbed << %r{http://yfrog.(com|ru|com.tr|it|fr|co.il|co.uk|com.pl|pl|eu|us)/(.*?)} # image & video hosting
+    OohEmbed << "http://*.xkcd.com/*" # A hilarious stick figure comic
+    OohEmbed << "http://*.wordpress.com/*/*/*/*" # Blogging Engine & community
+    OohEmbed << "http://*.wikipedia.org/wiki/*" # Online encyclopedia
+    OohEmbed << "http://*.twitpic.com/*" # Picture hosting for Twitter
+    OohEmbed << "http://twitter.com/*/statuses/*" # Mirco-blogging network
+    OohEmbed << "http://*.slideshare.net/*" # Share presentations online
+    OohEmbed << "http://*.phodroid.com/*/*/*" # Photo host
+    OohEmbed << "http://*.metacafe.com/watch/*" # Video host
+    OohEmbed << "http://video.google.com/videoplay?*" # Video hosting
+    OohEmbed << "http://*.funnyordie.com/videos/*" # Comedy video host
+    OohEmbed << "http://*.thedailyshow.com/video/*" # Syndicated show
+    OohEmbed << "http://*.collegehumor.com/video:*" # Comedic & original videos
+    OohEmbed << %r{http://(.*?).amazon.(com|co.uk|de|ca|jp)/(.*?)/(gp/product|o/ASIN|obidos/ASIN|dp)/(.*?)} # Online product shopping
+    OohEmbed << "http://*.5min.com/Video/*" # micro-video host
+
+    PollEverywhere = OEmbed::Provider.new("http://www.polleverywhere.com/services/oembed/")
+    PollEverywhere << "http://www.polleverywhere.com/polls/*"
+    PollEverywhere << "http://www.polleverywhere.com/multiple_choice_polls/*"
+    PollEverywhere << "http://www.polleverywhere.com/free_text_polls/*"
+
+    MyOpera = OEmbed::Provider.new("http://my.opera.com/service/oembed", :json)
+    MyOpera << "http://my.opera.com/*"
+
+    ClearspringWidgets = OEmbed::Provider.new("http://widgets.clearspring.com/widget/v1/oembed/")
+    ClearspringWidgets << "http://www.clearspring.com/widgets/*"
+
+    NFBCanada = OEmbed::Provider.new("http://www.nfb.ca/remote/services/oembed/")
+    NFBCanada << "http://*.nfb.ca/film/*"
+
+    Scribd = OEmbed::Provider.new("http://www.scribd.com/services/oembed")
+    Scribd << "http://*.scribd.com/*"
+
+    MovieCliips = OEmbed::Provider.new("http://movieclips.com/services/oembed/")
+    Movieclips << "http://movieclips.com/watch/*/*/"
+
+    TwentyThree << OEmbed::Provider.new("http://www.23hq.com/23/oembed")
+    TwentyThree << "http://www.23hq.com/*"
+
   end
 end
