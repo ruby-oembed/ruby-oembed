cef93b333432afa334b060ec2401abe3bdcf4272
Colin Shea <colin@evaryont.me>
2010-02-15 10:51:34 +0800

    Real-time registering of URLs and their providers

--- lib/oembed/provider.rb
+++ lib/oembed/provider.rb
@@ -12,12 +12,15 @@ module OEmbed
     def <<(url)
       if url.is_a? Regexp
         @urls << url
+        OEmbed::Providers.urls[url] = self
         return
       end
       full, scheme, domain, path = *url.match(%r{([^:]*)://?([^/?]*)(.*)})
       domain = Regexp.escape(domain).gsub("\\*", "(.*?)").gsub("(.*?)\\.", "([^\\.]+\\.)?")
       path = Regexp.escape(path).gsub("\\*", "(.*?)")
-      @urls << Regexp.new("^#{Regexp.escape(scheme)}://#{domain}#{path}")
+      url_regex = Regexp.new("^#{Regexp.escape(scheme)}://#{domain}#{path}")
+      @urls << url_regex
+      OEmbed::Providers.urls[url_regex] = self
     end
 
     def build(url, options = {})
