19f7ed6dd97e72d3b2a9bbbea816f694c6f3e60a
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2010-10-18 09:08:24 -0700

    A bit of testing for my previous commit.

--- spec/provider_spec.rb
+++ spec/provider_spec.rb
@@ -15,6 +15,137 @@ describe OEmbed::Provider do
     @viddler << "http://*.viddler.com/*"
   end
 
+  it "should require a valid endpoint for a new instance" do
+    proc { OEmbed::Provider.new("http://foo.com/oembed/") }.
+    should_not raise_error(ArgumentError)
+    
+    proc { OEmbed::Provider.new("https://foo.com/oembed/") }.
+    should_not raise_error(ArgumentError)
+  end
+  
+  it "should allow a {format} string in the endpoint for a new instance" do
+    proc { OEmbed::Provider.new("http://foo.com/oembed.{format}/get") }.
+    should_not raise_error(ArgumentError)
+  end
+  
+  it "should raise an ArgumentError given an invalid endpoint for a new instance" do
+    [
+      "httpx://foo.com/oembed/",
+      "ftp://foo.com/oembed/",
+      "foo.com/oembed/",
+      "http://not a uri",
+      nil, 1,
+    ].each do |endpoint|
+      #puts "given a endpoint of #{endpoint.inspect}"
+      proc { OEmbed::Provider.new(endpoint) }.
+      should raise_error(ArgumentError)
+    end
+  end
+
+  it "should allow no URI schema to be given" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    
+    provier.include?("http://foo.com/1").should be_true
+    provier.include?("http://bar.foo.com/1").should be_true
+    provier.include?("http://bar.foo.com/show/1").should be_true
+    provier.include?("https://bar.foo.com/1").should be_true
+    provier.include?("http://asdf.com/1").should be_true
+    provier.include?("asdf").should be_true
+  end
+
+  it "should allow a String as a URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    provier << "http://bar.foo.com/*"
+    
+    provier.include?("http://bar.foo.com/1").should be_true
+    provier.include?("http://bar.foo.com/show/1").should be_true
+    
+    provier.include?("https://bar.foo.com/1").should be_false
+    provier.include?("http://foo.com/1").should be_false
+  end
+  
+  it "should allow multiple path wildcards in a String URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    provier << "http://bar.foo.com/*/show/*"
+    
+    provier.include?("http://bar.foo.com/photo/show/1").should be_true
+    provier.include?("http://bar.foo.com/video/show/2").should be_true
+    provier.include?("http://bar.foo.com/help/video/show/2").should be_true
+    
+    provier.include?("https://bar.foo.com/photo/show/1").should be_false
+    provier.include?("http://foo.com/video/show/2").should be_false
+    provier.include?("http://bar.foo.com/show/1").should be_false
+    provier.include?("http://bar.foo.com/1").should be_false
+  end
+  
+  it "should NOT allow multiple domain wildcards in a String URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    
+    proc { provier << "http://*.com/*" }.
+    should raise_error(ArgumentError)
+    
+    provier.include?("http://foo.com/1").should be_false
+  end
+  
+  it "should allow a sub-domain wildcard in String URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    provier << "http://*.foo.com/*"
+    
+    provier.include?("http://bar.foo.com/1").should be_true
+    provier.include?("http://foo.foo.com/2").should be_true
+    provier.include?("http://foo.com/3").should be_true
+    
+    provier.include?("https://bar.foo.com/1").should be_false
+    provier.include?("http://my.bar.foo.com/1").should be_false
+    
+    provier << "http://my.*.foo.com/*"
+  end
+  
+  it "should allow multiple sub-domain wildcards in a String URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    provier << "http://*.my.*.foo.com/*"
+    
+    provier.include?("http://my.bar.foo.com/1").should be_true
+    provier.include?("http://my.foo.com/2").should be_true
+    provier.include?("http://bar.my.bar.foo.com/3").should be_true
+    
+    provier.include?("http://bar.foo.com/1").should be_false
+    provier.include?("http://foo.bar.foo.com/1").should be_false
+  end
+  
+  it "should NOT allow a scheme wildcard in a String URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    proc { provier << "*://foo.com/*" }.
+    should raise_error(ArgumentError)
+    
+    provier.include?("http://foo.com/1").should be_false
+  end
+
+  it "should allow a scheme other than http in a String URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    provier << "https://foo.com/*"
+    
+    provier.include?("https://foo.com/1").should be_true
+    
+    gopher_url = "gopher://foo.com/1"
+    provier.include?(gopher_url).should be_false
+    provier << "gopher://foo.com/*"
+    provier.include?(gopher_url).should be_true
+  end
+
+  it "should allow a Regexp as a URI schema" do
+    provier = OEmbed::Provider.new("http://foo.com/oembed")
+    provier << %r{^https?://([^\.]*\.)?foo.com/(show/)?\d+}
+    
+    provier.include?("http://bar.foo.com/1").should be_true
+    provier.include?("http://bar.foo.com/show/1").should be_true
+    provier.include?("http://foo.com/1").should be_true
+    provier.include?("https://bar.foo.com/1").should be_true
+    
+    provier.include?("http://bar.foo.com/video/1").should be_false
+    provier.include?("gopher://foo.com/1").should be_false
+  end
+
   it "should by default use OEmbed::Formatters::DEFAULT" do
     @flickr.format.should == @default
   end
