7e81817cda146238ce5692de1d24f8b909f464a4
Eliot Shepard <eshepard@slower.net>
2015-01-06 17:20:41 -0500

    Added provider for vine.co

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -179,6 +179,13 @@ module OEmbed
     Vimeo << "https://*.vimeo.com/*"
     add_official_provider(Vimeo)
 
+    # Provider for vine.co
+    # https://dev.twitter.com/web/vine/oembed
+    Vine = OEmbed::Provider.new("https://vine.co/oembed.{format}")
+    Vine << "http://*.vine.co/v/*"
+    Vine << "https://*.vine.co/v/*"
+    add_official_provider(Vine)
+
     # Provider for instagram.com
     # http://instagr.am/developer/embedding/
     Instagram = OEmbed::Provider.new("http://api.instagram.com/oembed", :json)
