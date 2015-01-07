81cd81c125101780dfcc8b485b90138f4a77007c
Marcos Wright-Kuhns <marcoswk@haikulearning.com>
2015-01-07 14:22:53 -0800

    Replace deprecated rspec `should` syntax
    ... in formatter specs

--- spec/formatter/json/yaml_backend_spec.rb
+++ spec/formatter/json/yaml_backend_spec.rb
@@ -4,51 +4,52 @@ describe "OEmbed::Formatter::JSON::Backends::Yaml" do
   include OEmbedSpecHelper
 
   before(:all) do
-    lambda {
+    expect {
       OEmbed::Formatter::JSON.backend = 'Yaml'
-    }.should_not raise_error
-    
-    (!!defined?(YAML)).should == true
+    }.to_not raise_error
+
+    expect(!!defined?(YAML)).to eq(true)
   end
 
   it "should support JSON" do
-    proc { OEmbed::Formatter.supported?(:json) }.
-    should_not raise_error
+    expect {
+      OEmbed::Formatter.supported?(:json)
+    }.to_not raise_error
   end
-  
+
   it "should be using the Yaml backend" do
-    OEmbed::Formatter::JSON.backend.should == OEmbed::Formatter::JSON::Backends::Yaml
+    expect(OEmbed::Formatter::JSON.backend).to eq(OEmbed::Formatter::JSON::Backends::Yaml)
   end
-  
+
   it "should decode a JSON String" do
     decoded = OEmbed::Formatter.decode(:json, valid_response(:json))
     # We need to compare keys & values separately because we don't expect all
     # non-string values to be recognized correctly.
-    decoded.keys.should == valid_response(:object).keys
-    decoded.values.map{|v|v.to_s}.should == valid_response(:object).values.map{|v|v.to_s}
+    expect(decoded.keys).to eq(valid_response(:object).keys)
+    expect(decoded.values.map{|v|v.to_s}).to eq(valid_response(:object).values.map{|v|v.to_s})
   end
-  
+
   it "should raise an OEmbed::ParseError when decoding an invalid JSON String" do
-    lambda {
+    expect {
       decode = OEmbed::Formatter.decode(:json, invalid_response('unclosed_container', :json))
-    }.should raise_error(OEmbed::ParseError)
-    lambda {
+    }.to raise_error(OEmbed::ParseError)
+    expect {
       decode = OEmbed::Formatter.decode(:json, invalid_response('unclosed_tag', :json))
-    }.should raise_error(OEmbed::ParseError)
-    lambda {
+    }.to raise_error(OEmbed::ParseError)
+    expect {
       decode = OEmbed::Formatter.decode(:json, invalid_response('invalid_syntax', :json))
-    }.should raise_error(OEmbed::ParseError)
+    }.to raise_error(OEmbed::ParseError)
   end
-  
+
   it "should raise an OEmbed::ParseError when decoding fails with an unexpected error" do
     error_to_raise = ArgumentError
-    OEmbed::Formatter::JSON.backend.parse_error.should_not be_kind_of(error_to_raise)
-    
-    OEmbed::Formatter::JSON.backend.should_receive(:convert_json_to_yaml).
-      and_raise(error_to_raise.new("unknown error"))
-    
-    lambda {
+    expect(OEmbed::Formatter::JSON.backend.parse_error).to_not be_kind_of(error_to_raise)
+
+    expect(OEmbed::Formatter::JSON.backend).to receive(:convert_json_to_yaml).
+      and_throw(error_to_raise.new("unknown error"))
+
+    expect {
       decode = OEmbed::Formatter.decode(:json, valid_response(:json))
-    }.should raise_error(OEmbed::ParseError)
+    }.to raise_error(OEmbed::ParseError)
   end
 end
--- spec/formatter/xml/nokogiri_backend_spec.rb
+++ spec/formatter/xml/nokogiri_backend_spec.rb
@@ -4,55 +4,56 @@ describe "OEmbed::Formatter::XML::Backends::Nokogiri" do
   include OEmbedSpecHelper
 
   before(:all) do
-    lambda {
+    expect {
       OEmbed::Formatter::XML.backend = 'Nokogiri'
-    }.should raise_error(LoadError)
-    
+    }.to raise_error(LoadError)
+
     require 'nokogiri'
-    
-    lambda {
+
+    expect {
       OEmbed::Formatter::XML.backend = 'Nokogiri'
-    }.should_not raise_error
+    }.to_not raise_error
   end
 
   it "should support XML" do
-    proc { OEmbed::Formatter.supported?(:xml) }.
-    should_not raise_error
+    expect {
+      OEmbed::Formatter.supported?(:xml)
+    }.to_not raise_error
   end
-  
+
   it "should be using the Nokogiri backend" do
-    OEmbed::Formatter::XML.backend.should == OEmbed::Formatter::XML::Backends::Nokogiri
+    expect(OEmbed::Formatter::XML.backend).to eq(OEmbed::Formatter::XML::Backends::Nokogiri)
   end
-  
+
   it "should decode an XML String" do
     decoded = OEmbed::Formatter.decode(:xml, valid_response(:xml))
     # We need to compare keys & values separately because we don't expect all
     # non-string values to be recognized correctly.
-    decoded.keys.should == valid_response(:object).keys
-    decoded.values.map{|v|v.to_s}.should == valid_response(:object).values.map{|v|v.to_s}
+    expect(decoded.keys).to eq(valid_response(:object).keys)
+    expect(decoded.values.map{|v|v.to_s}).to eq(valid_response(:object).values.map{|v|v.to_s})
   end
-  
+
   it "should raise an OEmbed::ParseError when decoding an invalid XML String" do
-    lambda {
+    expect {
       decode = OEmbed::Formatter.decode(:xml, invalid_response('unclosed_container', :xml))
-    }.should raise_error(OEmbed::ParseError)
-    lambda {
+    }.to raise_error(OEmbed::ParseError)
+    expect {
       decode = OEmbed::Formatter.decode(:xml, invalid_response('unclosed_tag', :xml))
-    }.should raise_error(OEmbed::ParseError)
-    lambda {
+    }.to raise_error(OEmbed::ParseError)
+    expect {
       decode = OEmbed::Formatter.decode(:xml, invalid_response('invalid_syntax', :xml))
-    }.should raise_error(OEmbed::ParseError)
+    }.to raise_error(OEmbed::ParseError)
   end
-  
+
   it "should raise an OEmbed::ParseError when decoding fails with an unexpected error" do
     error_to_raise = ArgumentError
-    OEmbed::Formatter::XML.backend.parse_error.should_not be_kind_of(error_to_raise)
-    
-    ::Nokogiri::XML::Document.should_receive(:parse).
+    expect(OEmbed::Formatter::XML.backend.parse_error).to_not be_kind_of(error_to_raise)
+
+    expect(::Nokogiri::XML::Document).to receive(:parse).
       and_raise(error_to_raise.new("unknown error"))
-    
-    lambda {
+
+    expect {
       decode = OEmbed::Formatter.decode(:xml, valid_response(:xml))
-    }.should raise_error(OEmbed::ParseError)
+    }.to raise_error(OEmbed::ParseError)
   end
 end
