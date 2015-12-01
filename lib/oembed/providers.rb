502fa89a2ae174097680886e33430c68d7fa6c4c
Javan Makhmali <javan@javan.us>
2015-12-01 17:50:54 -0500

    Match www Instagram subdomain

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -199,8 +199,10 @@ module OEmbed
     Instagram = OEmbed::Provider.new("https://api.instagram.com/oembed", :json)
     Instagram << "http://instagr.am/p/*"
     Instagram << "http://instagram.com/p/*"
+    Instagram << "http://www.instagram.com/p/*"
     Instagram << "https://instagr.am/p/*"
     Instagram << "https://instagram.com/p/*"
+    Instagram << "https://www.instagram.com/p/*"
     add_official_provider(Instagram)
 
     # Provider for slideshare.net
