e8a4429271c5ff4908b639d9506f5a3640dd6f93
John Crowe <thecrow1213@gmail.com>
2016-03-02 15:45:45 -0600

    Tweaks to provider initializations

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -207,18 +207,19 @@ module OEmbed
 
     # Providers for Facebook Posts & Videos
     # https://developers.facebook.com/docs/plugins/oembed-endpoints
-    FacebookPost = OEmbed::Provider.new('https://www.facebook.com/plugins/post/oembed.json/?url={format}')
+    FacebookPost = OEmbed::Provider.new('https://www.facebook.com/plugins/post/oembed.json/', :json)
     FacebookPost << 'https://www.facebook.com/*/posts/*'
     FacebookPost << 'https://www.facebook.com/*/activity/*'
     FacebookPost << 'https://www.facebook.com/photo*'
-    FacebookPost << 'https://www.facebook.com/photos/*'
+    FacebookPost << 'https://www.facebook.com/photos*'
+    FacebookPost << 'https://www.facebook.com/*/photos*'
     FacebookPost << 'https://www.facebook.com/permalink*'
-    FacebookPost << 'https://www.facebook.com/media/*'
-    FacebookPost << 'https://www.facebook.com/questions/*'
-    FacebookPost << 'https://www.facebook.com/notes/*'
+    FacebookPost << 'https://www.facebook.com/media*'
+    FacebookPost << 'https://www.facebook.com/questions*'
+    FacebookPost << 'https://www.facebook.com/notes*'
     add_official_provider(FacebookPost)
-    
-    FacebookVideo = OEmbed::Provider.new('https://www.facebook.com/plugins/video/oembed.json/?url={format}')
+
+    FacebookVideo = OEmbed::Provider.new('https://www.facebook.com/plugins/video/oembed.json/', :json)
     FacebookVideo << 'https://www.facebook.com/*/videos/*'
     FacebookVideo << 'https://www.facebook.com/video*'
     add_official_provider(FacebookVideo)
