ruby-oembed/ruby-oembed

commit 3d7ccfe47eec9fcdba90240ed3b943b8703fc24c

First run of `rubocop -a`

lib/oembed/provider.rb
@@ -27,7 +27,6 @@ module OEmbed
     # to get rid of it, though. --Marcos
     attr_accessor :url
 
-
     # Construct a new OEmbed::Provider instance, pointing at a specific oEmbed
     # endpoint.
     #
@@ -48,8 +47,12 @@ module OEmbed
     #   # "http://my.service.com/oembed.xml"
     #   @xml_provider = OEmbed::Provider.new("http://my.service.com/oembed.{format}", :xml)
     def initialize(endpoint, format = OEmbed::Formatter.default)
-      endpoint_uri = URI.parse(endpoint.gsub(/[\{\}]/,'')) rescue nil
-      raise ArgumentError, "The given endpoint isn't a valid http(s) URI: #{endpoint.to_s}" unless endpoint_uri.is_a?(URI::HTTP)
+      endpoint_uri = begin
+                       URI.parse(endpoint.gsub(/[\{\}]/, ''))
+                     rescue
+                       nil
+                     end
+      fail ArgumentError, "The given endpoint isn't a valid http(s) URI: #{endpoint}" unless endpoint_uri.is_a?(URI::HTTP)
 
       @endpoint = endpoint
       @urls = []
@@ -66,10 +69,10 @@ module OEmbed
     #   @provider << "http://*.service.com/photo/*/slideshow"
     #   @provider << %r{^http://my.service.com/((help)|(faq))/\d+[#\?].*}
     def <<(url)
-      if !url.is_a?(Regexp)
+      unless url.is_a?(Regexp)
         full, scheme, domain, path = *url.match(%r{([^:]*)://?([^/?]*)(.*)})
-        domain = Regexp.escape(domain).gsub("\\*", "(.*?)").gsub("(.*?)\\.", "([^\\.]+\\.)?")
-        path = Regexp.escape(path).gsub("\\*", "(.*?)")
+        domain = Regexp.escape(domain).gsub('\\*', '(.*?)').gsub('(.*?)\\.', '([^\\.]+\\.)?')
+        path = Regexp.escape(path).gsub('\\*', '(.*?)')
         url = Regexp.new("^#{Regexp.escape(scheme)}://#{domain}#{path}")
       end
       @urls << url
@@ -93,14 +96,14 @@ module OEmbed
     # Determine whether the given url is supported by this Provider by matching
     # against the Provider's URL schemes.
     def include?(url)
-      @urls.empty? || !!@urls.detect{ |u| u =~ url }
+      @urls.empty? || !!@urls.detect { |u| u =~ url }
     end
 
     # @deprecated *Note*: This method will be made private in the future.
     def build(url, query = {})
-      raise OEmbed::NotFound, url unless include?(url)
+      fail OEmbed::NotFound, url unless include?(url)
 
-      query = query.merge({:url => ::CGI.escape(url)})
+      query = query.merge(:url => ::CGI.escape(url))
       query.delete(:timeout)
       query.delete(:max_redirects)
 
@@ -109,21 +112,19 @@ module OEmbed
 
       endpoint = @endpoint.clone
 
-      if endpoint.include?("{format}")
-        endpoint["{format}"] = this_format
+      if endpoint.include?('{format}')
+        endpoint['{format}'] = this_format
         query.delete(:format)
       end
 
       base = endpoint.include?('?') ? '&' : '?'
-      query = base + query.inject("") do |memo, (key, value)|
+      query = base + query.inject('') do |memo, (key, value)|
         "#{key}=#{value}&#{memo}"
       end.chop
 
       URI.parse(endpoint + query).instance_eval do
         @format = this_format
-        def format
-          @format
-        end
+        attr_reader :format
         self
       end
     end

Gemfile
@@ -1,4 +1,4 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Travis CI was running into several bundler-related bug
 # * https://github.com/bundler/bundler/pull/3559
@@ -11,8 +11,8 @@ gem 'bundler', '~>1.10'
 gemspec
 
 group :guard do
-  gem "guard-rspec"
-  gem "guard-bundler"
-  gem "guard-rubocop"
-  gem "rb-fsevent"
+  gem 'guard-rspec'
+  gem 'guard-bundler'
+  gem 'guard-rubocop'
+  gem 'rb-fsevent'
 end

Guardfile
@@ -1,11 +1,11 @@
-guard "bundler" do
-  watch("Gemfile")
+guard 'bundler' do
+  watch('Gemfile')
 end
 
 group :red_green_refactor, :halt_on_fail => true do
-  guard "rspec", :cmd => "bundle exec rspec" do
+  guard 'rspec', :cmd => 'bundle exec rspec' do
     watch(%r{^spec/.+_spec\.rb$})
-    watch(%r{^spec/cassettes/.+.yml$}) { "spec" }
+    watch(%r{^spec/cassettes/.+.yml$}) { 'spec' }
     watch(%r{^lib/(.+)\.rb$}) do |m|
       # Split up the file path into an Array
       path_parts = []
@@ -22,18 +22,18 @@ group :red_green_refactor, :halt_on_fail => true do
       # Special case for formatter specs
       if path_parts.include?('formatter') && path_parts.include?('backends')
         path_parts.delete('backends')
-        path_parts.last.gsub!(/$/, "_backend")
+        path_parts.last.gsub!(/$/, '_backend')
       end
       # Add on the _spec.rb postfix
-      path_parts.last.gsub!(/$/, "_spec.rb")
+      path_parts.last.gsub!(/$/, '_spec.rb')
 
-      f = File.join("spec", *path_parts)
+      f = File.join('spec', *path_parts)
       puts "#{m.inspect} => #{f.inspect}"
       f
     end
   end
 
-  guard :rubocop, :cmd => "bundle exec rubocop" do
+  guard :rubocop, :cmd => 'bundle exec rubocop' do
     watch(/.+\.rb$/)
     watch(%r{(?:.+/)?\.rubocop\.yml$}) { |m| File.dirname(m[0]) }
   end

Rakefile
@@ -1,7 +1,7 @@
 begin
-  require "bundler/gem_tasks"
+  require 'bundler/gem_tasks'
 rescue LoadError
-  puts "Bundler not available. Install it with: gem install bundler"
+  puts 'Bundler not available. Install it with: gem install bundler'
 end
 
-Dir[File.join(File.dirname(__FILE__), "lib/tasks/*.rake")].sort.each { |ext| load ext }
+Dir[File.join(File.dirname(__FILE__), 'lib/tasks/*.rake')].sort.each { |ext| load ext }

integration_test/test.rb
@@ -1,31 +1,30 @@
 require 'rubygems'
 require File.dirname(__FILE__) + '/../lib/oembed'
-OEmbed::Providers.register_all()
+OEmbed::Providers.register_all
 OEmbed::Providers.register_fallback(OEmbed::ProviderDiscovery, OEmbed::Providers::Embedly, OEmbed::Providers::OohEmbed)
 
-
-passed = "passed"
-passed = "failed"
-File.open("test_urls.csv", "r") do |infile|
-    while (line = infile.gets)
-        begin
-            res = OEmbed::Providers.raw(line, :format => :json)
-            passed = "passed"
-        rescue OEmbed::NotFound => e
-            if e.message == "OEmbed::NotFound"
-                puts "not a supported url: " + line
-            else
-                puts e.message
-            end
-            passed = "failed"
-        rescue OEmbed::UnknownResponse => e
-            puts "got a bad network response" + e.message
-            passed = "failed"
-        rescue Timeout::Error
-            puts "timeout error"
-            passed = "failed"
-        end
-        
-        puts passed + ": " + line
+passed = 'passed'
+passed = 'failed'
+File.open('test_urls.csv', 'r') do |infile|
+  while (line = infile.gets)
+    begin
+      res = OEmbed::Providers.raw(line, :format => :json)
+      passed = 'passed'
+    rescue OEmbed::NotFound => e
+      if e.message == 'OEmbed::NotFound'
+        puts 'not a supported url: ' + line
+      else
+        puts e.message
+      end
+      passed = 'failed'
+    rescue OEmbed::UnknownResponse => e
+      puts 'got a bad network response' + e.message
+      passed = 'failed'
+    rescue Timeout::Error
+      puts 'timeout error'
+      passed = 'failed'
     end
+
+    puts passed + ': ' + line
+  end
 end

lib/oembed.rb
@@ -1,4 +1,4 @@
-$:.unshift File.dirname(__FILE__)
+$LOAD_PATH.unshift File.dirname(__FILE__)
 
 require 'net/http'
 

lib/oembed/errors.rb
@@ -1,5 +1,4 @@
 module OEmbed
-  
   # A generic OEmbed-related Error. The OEmbed library does its best to capture all internal
   # errors and wrap them in an OEmbed::Error class so that the error-handling code in your
   # application can more easily identify the source of errors.
@@ -43,5 +42,4 @@ module OEmbed
       "There was an error parsing the server response (#{super})"
     end
   end
-    
 end

lib/oembed/formatter.rb
@@ -5,15 +5,20 @@ require 'oembed/formatter/xml'
 module OEmbed
   # Takes the raw response from an oEmbed server and turns it into a nice Hash of data.
   module Formatter
-    
     class << self
       # Returns the default format for OEmbed::Provider requests as a String.
       def default
         # Listed in order of preference.
-        %w{json xml}.detect { |type| supported?(type) rescue false }
+        %w(json xml).detect do |type|
+          begin
+                                       supported?(type)
+                                     rescue
+                                       false
+                                     end
+        end
       end
-      
-      # Given the name of a format we want to know about (e.g. 'json'), returns 
+
+      # Given the name of a format we want to know about (e.g. 'json'), returns
       # true if there is a valid backend. If there is no backend, raises
       # OEmbed::FormatNotSupported.
       def supported?(format)
@@ -23,58 +28,58 @@ module OEmbed
         when 'xml'
           XML.supported?
         else
-          raise OEmbed::FormatNotSupported, format
+          fail OEmbed::FormatNotSupported, format
         end
       end
 
       # Convert the given value into a nice Hash of values. The format should
       # be the name of the response format (e.g. 'json'). The value should be
       # a String or IO containing the response from an oEmbed server.
-      # 
+      #
       # For example:
       #   value = '{"version": "1.0", "type": "link", "title": "Some Cool News Article"}'
       #   OEmbed::Formatter.decode('json', value)
       #   #=> {"version": "1.0", "type": "link", "title": "Some Cool News Article"}
       def decode(format, value)
         supported?(format)
-        
+
         begin
           case format.to_s
           when 'json'
             begin
               JSON.decode(value)
             rescue JSON.backend.parse_error
-              raise OEmbed::ParseError, $!.message
+              raise OEmbed::ParseError, $ERROR_INFO.message
             end
           when 'xml'
             begin
               XML.decode(value)
             rescue XML.backend.parse_error
-              raise OEmbed::ParseError, $!.message
+              raise OEmbed::ParseError, $ERROR_INFO.message
             end
           end
         rescue
-          raise OEmbed::ParseError, "#{$!.class}: #{$!.message}"
+          raise OEmbed::ParseError, "#{$ERROR_INFO.class}: #{$ERROR_INFO.message}"
         end
       end
-      
+
       # Test the given backend to make sure it parses known values correctly.
       # The backend_module should be either a JSON or XML backend.
       def test_backend(backend_module)
         expected = {
-          "version"=>1.0,
-          "string"=>"test",
-          "int"=>42,
-          "html"=>"<i>Cool's</i>\n the \"word\"!",
+          'version' => 1.0,
+          'string' => 'test',
+          'int' => 42,
+          'html' => "<i>Cool's</i>\n the \"word\"!"
         }
-        
+
         given_value = case backend_module.to_s
-        when /OEmbed::Formatter::JSON::Backends::/
-          <<-JSON
+                      when /OEmbed::Formatter::JSON::Backends::/
+                        <<-JSON
 {"version":"1.0", "string":"test", "int":42,"html":"<i>Cool's</i>\\n the \\"word\\"\\u0021"}
           JSON
-        when /OEmbed::Formatter::XML::Backends::/
-          <<-XML
+                      when /OEmbed::Formatter::XML::Backends::/
+                        <<-XML
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <oembed>
   <version>1.0</version>
@@ -83,20 +88,16 @@ module OEmbed
   <html>&lt;i&gt;Cool's&lt;/i&gt;\n the &quot;word&quot;&#x21;</html>
 </oembed>
           XML
-        else
-          nil
-        end
-        
+                      end
+
         actual = backend_module.decode(given_value)
-        
+
         # For the test to be true the actual output Hash should have the
         # exact same list of keys _and_ the values should be the same
         # if we ignoring typecasting.
         actual.keys.sort == expected.keys.sort &&
           !actual.detect { |key, value| value.to_s != expected[key].to_s }
       end
-      
     end # self
-    
   end
 end

commit 14cfb72c0c61697b599011802798fec3adf7549f

Fix OEmbed::Provider (broken in 3d7ccfe47eec9fcdb)
Stop adding URI#format accessor as part of the Provider#build method.

That modified URI isn't made available anywhere & the format used to get a particular OEmbed::Response is available via the Response#format method anyway.

lib/oembed/provider.rb
@@ -70,7 +70,7 @@ module OEmbed
     #   @provider << %r{^http://my.service.com/((help)|(faq))/\d+[#\?].*}
     def <<(url)
       unless url.is_a?(Regexp)
-        full, scheme, domain, path = *url.match(%r{([^:]*)://?([^/?]*)(.*)})
+        _full, scheme, domain, path = *url.match(%r{([^:]*)://?([^/?]*)(.*)})
         domain = Regexp.escape(domain).gsub('\\*', '(.*?)').gsub('(.*?)\\.', '([^\\.]+\\.)?')
         path = Regexp.escape(path).gsub('\\*', '(.*?)')
         url = Regexp.new("^#{Regexp.escape(scheme)}://#{domain}#{path}")
@@ -122,11 +122,7 @@ module OEmbed
         "#{key}=#{value}&#{memo}"
       end.chop
 
-      URI.parse(endpoint + query).instance_eval do
-        @format = this_format
-        attr_reader :format
-        self
-      end
+      URI.parse(endpoint + query)
     end
 
     # @deprecated *Note*: This method will be made private in the future.