f7073e344172b6979a294f75fe97f3e2b2d987cd
Ben Ramsey <ben@benramsey.com>
2016-05-12 21:30:07 -0500

    Set JSON as the default formatter for Twitter

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -183,7 +183,7 @@ module OEmbed
 
     # Provider for twitter.com
     # https://dev.twitter.com/rest/reference/get/statuses/oembed
-    Twitter = OEmbed::Provider.new("https://publish.twitter.com/oembed")
+    Twitter = OEmbed::Provider.new("https://publish.twitter.com/oembed", :json)
     Twitter << "https://*.twitter.com/*/status/*"
     add_official_provider(Twitter)
 
