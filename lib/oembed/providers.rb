09c9af97f439495250208dd31b8a3cf976be2c78
Inge Jørgensen <inge@anyone.no>
2020-10-04 20:10:18 +0200

    Move aggregator lists out of the way

--- lib/oembed/providers.rb
+++ lib/oembed/providers.rb
@@ -390,7 +390,7 @@ module OEmbed
     Noembed = OEmbed::Provider.new("https://noembed.com/embed")
     # Add all known URL regexps for Noembed.
     # To update this list run `rake oembed:update_noembed`
-    YAML.load_file(File.join(File.dirname(__FILE__), "/providers/noembed_urls.yml")).each do |url|
+    YAML.load_file(File.join(File.dirname(__FILE__), "/providers/aggregators/noembed_urls.yml")).each do |url|
       Noembed << Regexp.new(url)
     end
     add_official_provider(Noembed, :aggregators)
@@ -405,7 +405,7 @@ module OEmbed
     # If you don't yet have an API key you'll need to sign up here: http://embed.ly/pricing
     Embedly = OEmbed::Provider.new("http://api.embed.ly/1/oembed")
     # Add all known URL regexps for Embedly. To update this list run `rake oembed:update_embedly`
-    YAML.load_file(File.join(File.dirname(__FILE__), "/providers/embedly_urls.yml")).each do |url|
+    YAML.load_file(File.join(File.dirname(__FILE__), "/providers/aggregators/embedly_urls.yml")).each do |url|
       Embedly << url
     end
     add_official_provider(Embedly, :aggregators)
