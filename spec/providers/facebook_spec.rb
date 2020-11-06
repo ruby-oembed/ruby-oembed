d8965e266ee227df0fda07839b529891f59798e7
Marcos Wright-Kuhns <mwrightkuhns@newrelic.com>
2020-11-05 21:53:15 -0800

    Move the tests for Providers.register_all to the providers_spec
    Also test to ensure Facebook providers are NOT registered by default.

--- spec/providers/facebook_spec.rb
+++ spec/providers/facebook_spec.rb
@@ -2,10 +2,10 @@ require File.join(File.dirname(__FILE__), '../spec_helper')
 
 describe 'Facebook providers' do
   let(:access_token) { 'my-fake-access-token' }
-  let(:embed_url) { 'https://www.facebook.com/exampleuser/posts/1234567890' }
 
   describe 'FacebookPost provider' do
     let(:provider) { OEmbed::Providers::FacebookPost.new(access_token: access_token) }
+    let(:embed_url) { 'https://www.facebook.com/exampleuser/posts/1234567890' }
 
     it 'sets the endpoint URL' do
       expect(provider.endpoint).to(
@@ -45,41 +45,6 @@ describe 'Facebook providers' do
 
     it 'recognizes embed URLs' do
       expect(provider).to include(embed_url)
-    end
-  end
-
-  describe 'registering as default provider' do
-    around(:each) do |each|
-      previous_value = ENV['OEMBED_FACEBOOK_TOKEN']
-      ENV['OEMBED_FACEBOOK_TOKEN'] = nil
-      each.run
-      ENV['OEMBED_FACEBOOK_TOKEN'] = previous_value
-      OEmbed::Providers.unregister_all
-    end
-
-    subject { OEmbed::Providers.find(embed_url) }
-
-    context 'when access token is provided to register_all' do
-      before do
-        OEmbed::Providers.register_all(access_tokens: { facebook: access_token })
-      end
-
-      it { is_expected.to be_a(OEmbed::Providers::FacebookPost) }
-    end
-
-    context 'when access token is set as an environment variable' do
-      before do
-        ENV['OEMBED_FACEBOOK_TOKEN'] = access_token
-        OEmbed::Providers.register_all
-      end
-
-      it { is_expected.to be_a(OEmbed::Providers::FacebookPost) }
-    end
-
-    context 'without access token' do
-      before { OEmbed::Providers.register_all }
-
-      it { is_expected.to eq(nil) }
     end
   end
 end
