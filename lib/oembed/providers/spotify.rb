71ac822019c8c352504eec28bf643ad478358805
Marcos Wright-Kuhns <webmaster@wrightkuhns.com>
2024-11-25 17:48:25 -0800

    Switch to the new spotify oembed API endpoint
    ...which is now officially documented on Spotify's website!

--- lib/oembed/providers/spotify.rb
+++ lib/oembed/providers/spotify.rb
@@ -1,8 +1,12 @@
 module OEmbed
   class Providers
     # Provider for spotify.com
-    # See https://developer.spotify.com/documentation/embeds/tutorials/using-the-oembed-api
-    Spotify = OEmbed::Provider.new("https://embed.spotify.com/oembed/")
+    # https://developer.spotify.com/documentation/embeds/reference/oembed
+    # https://developer.spotify.com/documentation/embeds/tutorials/using-the-oembed-api
+    Spotify = OEmbed::Provider.new(
+      "https://open.spotify.com/oembed",
+      format: :json
+    )
     Spotify << "http://open.spotify.com/*"
     Spotify << "https://open.spotify.com/*"
     Spotify << "http://play.spotify.com/*"
